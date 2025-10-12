8dad044b1ca3d205ceeabd3391b24184202d6272
Tuhin Paul <tntuhin369paul@gmail.com>
2025-10-12 20:19:18 +0530

    fix

--- models/Material.js
+++ models/Material.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const materialSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  fileUrl: { type: String }, // optional: for uploaded files or links
+  fileUrl: { type: String }, 
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
 });
